421d4370461d8c4e952827c218d1c78d3d99c45b
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-06-11 10:06:29 -0500

    :bug: Fixed test

--- tests/main_test.py
+++ tests/main_test.py
@@ -5,7 +5,7 @@ import os
 # Add the src directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-import src.main import app
+from src.main import app
 
 @pytest.fixture
 def client():
