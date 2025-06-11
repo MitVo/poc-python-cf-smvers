39040fefe5f0b72b42bc0a47aa9308c40ae71690
mitzyvalenciaordonez <mitzyvo@gmail.com>
2025-06-11 09:42:00 -0500

    :sparkles: Added cache deps

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 import src.main
 
 @pytest.fixture
